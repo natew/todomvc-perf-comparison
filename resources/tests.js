9bb442f26839053b1acf00ab739558f704ec5e0f
natew <natewienert@gmail.com>
2015-04-26 17:09:57 -0700

    add reapp

--- resources/tests.js
+++ resources/tests.js
@@ -1,4 +1,4 @@
-var numberOfItemsToAdd = 100;
+var numberOfItemsToAdd = 500;
 var Suites = [];
 
 Suites.push({
@@ -124,6 +124,40 @@ Suites.push({
     version: '0.10.0',
     prepare: function (runner, contentWindow, contentDocument) {
         contentWindow.Utils.store = function () {}
+        return runner.waitForElement('#new-todo').then(function (element) {
+            element.focus();
+            return element;
+        });
+    },
+    tests: [
+        new BenchmarkTestStep('Adding' + numberOfItemsToAdd + 'Items', function (newTodo, contentWindow, contentDocument) {
+            for (var i = 0; i < numberOfItemsToAdd; i++) {
+                var keydownEvent = document.createEvent('Event');
+                keydownEvent.initEvent('keydown', true, true);
+                keydownEvent.which = 13; // VK_ENTER
+                newTodo.value = 'Something to do ' + i;
+                newTodo.dispatchEvent(keydownEvent);
+            }
+        }),
+        new BenchmarkTestStep('CompletingAllItems', function (newTodo, contentWindow, contentDocument) {
+            var checkboxes = contentDocument.querySelectorAll('.toggle');
+            for (var i = 0; i < checkboxes.length; i++)
+                checkboxes[i].click();
+        }),
+        new BenchmarkTestStep('DeletingAllItems', function (newTodo, contentWindow, contentDocument) {
+            var deleteButtons = contentDocument.querySelectorAll('.destroy');
+            for (var i = 0; i < deleteButtons.length; i++)
+                deleteButtons[i].click();
+        })
+    ]
+});
+
+Suites.push({
+    name: 'Reapp',
+    url: 'todomvc/reapp/index.html',
+    version: '1.0.0',
+    prepare: function (runner, contentWindow, contentDocument) {
+        // contentWindow.Utils.store = function () {}
         return runner.waitForElement('#new-todo').then(function (element) {
             element.focus();
             return element;
